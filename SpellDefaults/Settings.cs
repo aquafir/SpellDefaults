070af02d651949054c11919a4c7b01952f9da2ce
Aqua Fir <aquafirs@gmail.com>
2024-06-29 15:42:58 -0500

    Add project files.

---
+++ SpellDefaults/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace SpellDefaults;
+
+public class Settings
+{
+    public List<SpellId> Spells { get; set; } = Enum.GetValues<SpellId>().Where(x => x.ToString().Contains("Paragon")).ToList();
+    public double Duration { get; set; } = TimeSpan.FromHours(24).TotalSeconds;
+}
